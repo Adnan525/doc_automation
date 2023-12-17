e3b40544799798db850289cbefebdc5562275a5d
Muntasir Adnan <48549305+Adnan525@users.noreply.github.com>
2023-12-17 14:01:52 +0000

    can UPDATE date and day

---
+++ main.py
@@ -0,0 +1,54 @@
+from docx import Document
+from datetime import datetime
+
+ # dd-mm-yyyy format
+current_date = datetime.now().strftime("%d-%m-%Y")
+# day
+current_day = datetime.now().strftime("%A")
+
+def _print_table_contents(table):
+    for row_idx, row in enumerate(table.rows):
+        for col_idx, cell in enumerate(row.cells):
+            print(f"Row {row_idx}, Column {col_idx}: {cell.text}")
+
+def _update_table_cell(table, row_index, col_index, new_value):
+    """
+        Row 1, Column 0: DATE
+        Row 1, Column 3: DAY
+        Row 2, Column 0: SECURITY OFFICER
+        Row 2, Column 2: LICENSE NO.
+        Row 3, Column 0: START TIME
+        Row 3, Column 2: FINISH TIME
+
+    """
+    table.cell(row_index, col_index).text = new_value
+
+def update_template(template_path, output_path):
+
+    # template
+    template_doc = Document(template_path)
+
+    # Update date and day in the first table
+    first_table = template_doc.tables[0]
+    # _print_table_contents(first_table)
+    _update_table_cell(first_table, 1, 1, current_date)
+    _update_table_cell(first_table, 1, 3, current_day)
+
+    # Save the modified document
+    template_doc.save(output_path)
+
+if __name__ == "__main__":
+
+    # template document
+    template_path = "template.docx"
+
+    # output file
+    output_summary = "output_document.docx"
+    # date file
+    output_shift = f"{current_date}.docx"
+
+    # Update the template and save the modified document
+    update_template(template_path, output_summary)
+    update_template(template_path, output_shift)
+
+    print(f"Modified document saved as: {output_summary} and {output_shift}")
